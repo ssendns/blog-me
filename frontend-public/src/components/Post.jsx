8357573cb7fd5fee5f4e56e18874ca578d521b4d
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-08-29 18:56:41 +0300

    add image display and better post preview

--- frontend-public/src/components/Post.jsx
+++ frontend-public/src/components/Post.jsx
@@ -1,18 +1,40 @@
-import "../assets/post.css";
+import "../assets/postList.css";
 import React from "react";
 
 export default function Post({ post }) {
+  const currentUser = localStorage.getItem("username");
+
+  const hasImage = !!post.imageUrl?.trim();
+  const maxLength = hasImage ? 100 : 500;
+  const preview =
+    post.content.length > maxLength
+      ? post.content.slice(0, maxLength) + "..."
+      : post.content;
+
   return (
     <li className="post-card">
-      <div className="image-container">
-        <img src="/image.png" alt="post" className="post-image" />
-      </div>
+      {hasImage && (
+        <div className="image-container">
+          <img src={post.imageUrl} alt="post" className="post-image" />
+        </div>
+      )}
       <div className="post-content">
-        <a href={`/${post.id}`}>
-          <strong>{post.title}</strong>
-        </a>
-        <p className="post-author">by {post.authorName || "anon"}</p>
-        <p className="post-snippet">{post.content.slice(0, 500)}</p>
+        <div className="post-content-text">
+          <a href={`/posts/${post.id}`}>
+            <strong>
+              {post.title} {!post.published && <span className="lock">🔒</span>}
+            </strong>
+          </a>
+          <p className="post-author">by {post.authorName || "anon"}</p>
+          <p className="post-snippet">{preview}</p>
+        </div>
+        <div className="post-content-link">
+          {post.authorName === currentUser && (
+            <a className="edit-link" href={`/edit/${post.id}`}>
+              edit
+            </a>
+          )}
+        </div>
       </div>
     </li>
   );
